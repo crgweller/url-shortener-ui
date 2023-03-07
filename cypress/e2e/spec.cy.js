94254a3298de31bffd78df71889c83325556c9a7
Craig Weller <crg.weller@gmail.com>
2023-03-07 10:42:44 -0700

    iteration3/add-JSON-file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,24 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("landing page", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/urls", {
+      statusCode: 200,
+      ok: true,
+      fixture: "urls.json",
+    });
+
+    cy.visit("http://localhost:3000");
+  });
+
+  it("should display a header title", () => {
+    cy.get("h1").should("be.visible").contains("URL Shortener");
+  });
+
+  it("should display a form with a title input, url input, and submit button", () => {
+    cy.get("form").should("be.visible");
+    cy.get("input").should("have.length", 2);
+    cy.get("button").should("be.visible").contains("Shorten Please!");
+  });
+
+  
+
+});
